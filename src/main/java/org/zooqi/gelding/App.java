3a57559b5b7d98c5b909e66c3324fc5871c30e85
Judge <zooqih@gmail.com>
2017-08-04 09:54:24 +0800

    Initial project

---
+++ src/main/java/org/zooqi/gelding/App.java
@@ -0,0 +1,13 @@
+package org.zooqi.gelding;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Gelding!");
+    }
+}
